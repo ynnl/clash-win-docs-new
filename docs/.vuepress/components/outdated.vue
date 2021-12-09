97af094366300bc0cb697d6bb1d030b6c5e430a3
Fndroid <fndroid@icloud.com>
2021-12-09 11:22:17 +0800

    fix: slot click toggle hide

--- docs/.vuepress/components/outdated.vue
+++ docs/.vuepress/components/outdated.vue
@@ -1,6 +1,8 @@
 <template>
-  <div @click="show = !show" class="outdate-content">
-    <div class="hint">部分内容已于 v{{ since }} 后过期，点击查看</div>
+  <div class="outdate-content">
+    <div @click="show = !show" class="hint">
+      部分内容已于 v{{ since }} 后过期，点击查看
+    </div>
     <slot v-if="show" />
   </div>
 </template>
@@ -22,14 +24,17 @@ export default {
 <style scoped>
 .outdate-content {
   border: 1px solid #e6e6e6;
-  padding: 10px;
   border-radius: 5px;
+  padding: 0 10px;
 }
 
 .hint {
+  padding: 10px 0;
+  user-select: none;
   text-align: center;
   color: rgb(102, 102, 102);
   font-size: 14px;
+  width: 100%;
   cursor: pointer;
 }
 </style>
